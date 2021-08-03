664d2ee5046bdcb03dbe63fc1f9b3951a6e609d3
GitHubのユーザ名kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-08-03 21:40:44 +0000

    [Add] 商品一覧画面

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,22 @@
+class OrdersController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def thanks
+  end
+
+  def confirm
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+
+  devise_for :admins
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :orders
+    resources :customers
+    resources :genres
+    resources :items
+    resources :orders
+    resources :order_details
+  end
+
+  get 'cart_items/destroy_all'
+  get 'orders/thanks'
+  get 'orders/confirm'
+  resources :items
+  resources :cart_items
+  resources :orders
+  resources :addresses
+
 end
